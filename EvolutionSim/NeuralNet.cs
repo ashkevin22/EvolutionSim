d18cf415c8b93ae6cfbf6bcbe1c57edec4d4616f
ashkevin22 <ashkevin22@gmail.com>
2023-04-15 21:15:08 -0600

    Neural net somewhat done
    Except I'm really stupid and made it a supervised learning neural net instead of unsupervised so it's gonna have to all be changed :(

---
+++ EvolutionSim/NeuralNet.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection.Emit;
+
+namespace EvolutionSim
+{
+    public class NeuralNet
+    {
+        private List<List<Neuron>> m_layers = new();
+        private double m_error;
+        private double m_recentAverageError;
+        private double m_recentAverageSmoothingFactor;
+
+        /// <summary>
+        /// Constructor for NeuralNet, creates a neural network based on the topology List
+        /// </summary>
+        /// <param name="topology">List describing the topology of the neural network</param>
+        public NeuralNet(List<int> topology)
+        {
+            int numLayers = topology.Count;
+            for(int layerNum = 0; layerNum < numLayers; layerNum++)
+            {
+                List<Neuron> newLayer = new();
+                int numOutputs = layerNum == topology.Count() - 1 ? 0 : topology[layerNum + 1];
+
+                for(int neuronNum = 0; neuronNum <= topology[layerNum]; neuronNum++)
+                {
+                    newLayer.Add(new Neuron(numOutputs, neuronNum));
+                }
+                newLayer.Last().m_outputVal = 1.0;
+                m_layers.Add(newLayer);
+            }
+        }
+
+        /// <summary>
+        /// Function to feed forward the input values through the entire neural network
+        /// </summary>
+        /// <param name="inputVals">Initial input values for the neural network</param>
+        public void FeedForward(List<double> inputVals)
+        {
+            Debug.Assert(inputVals.Count == m_layers[0].Count - 1);
+
+            for(int i = 0; i < inputVals.Count; i++)
+            {
+                m_layers[0][i].m_outputVal = inputVals[i];
+            }
+
+            for(int layerNum = 1; layerNum < m_layers.Count; layerNum++)
+            {
+                List<Neuron> prevLayer = m_layers[layerNum - 1];
+                for(int i = 0; i < m_layers[layerNum].Count; i++)
+                {
+                    m_layers[layerNum][i].FeedForward(prevLayer);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Function to propogate backwards through the neural network and adjust all of the weights
+        /// </summary>
+        /// <param name="targetVals">expected outputs of the neural network</param>
+        public void BackProp(List<double> targetVals)
+        {
+            List<Neuron> outputLayer = m_layers.Last();
+            m_error = 0.0;
+
+            for (int n = 0; n < outputLayer.Count - 1; ++n)
+            {
+                double delta = targetVals[n] - outputLayer[n].m_outputVal;
+                m_error += delta * delta;
+            }
+            m_error /= outputLayer.Count - 1; // get average error squared
+            m_error = Math.Sqrt(m_error); // RMS
+
+            m_recentAverageError =
+                    (m_recentAverageError * m_recentAverageSmoothingFactor + m_error)
+                    / (m_recentAverageSmoothingFactor + 1.0);
+
+            for (int n = 0; n < outputLayer.Count - 1; ++n)
+            {
+                outputLayer[n].CalcOutputGradients(targetVals[n]);
+            }
+
+            for (int layerNum = m_layers.Count - 2; layerNum > 0; --layerNum)
+            {
+                List<Neuron> hiddenLayer = m_layers[layerNum];
+                List<Neuron> nextLayer = m_layers[layerNum + 1];
+
+                for (int n = 0; n < hiddenLayer.Count; ++n)
+                {
+                    hiddenLayer[n].CalcHiddenGradients(nextLayer);
+                }
+            }
+
+            for (int layerNum = m_layers.Count - 1; layerNum > 0; --layerNum)
+            {
+                List<Neuron> layer = m_layers[layerNum];
+                List<Neuron> prevLayer = m_layers[layerNum - 1];
+
+                for (int n = 0; n < layer.Count - 1; ++n)
+                {
+                    layer[n].UpdateInputWeights(prevLayer);
+                }
+            }
+        }
+    }
+}
---
+++ EvolutionSim/Neuron.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace EvolutionSim
+{
+	public class Connection
+	{
+		public double Weight;
+		public double DeltaWeight = 0;
+
+		public Connection(double weight)
+		{
+			this.Weight = weight;
+		}
+	}
+
+	public class Neuron
+	{
+		public double m_outputVal;
+
+		private static double Eta = 0.15;
+		private static double Alpha = 0.5;
+		private List<Connection> m_outputWeights;
+		private int m_myIndex;
+		private double m_gradient;
+
+		/// <summary>
+		/// Constructor that creates a neuron based on number of outputs
+		/// </summary>
+		/// <param name="numOutputs">number of outputs for the neuron</param>
+		/// <param name="myIndex">index of neuron</param>
+		public Neuron(int numOutputs, int myIndex)
+		{
+			for (int i = 0; i < numOutputs; i++)
+			{
+				m_outputWeights.Add(new Connection(RandomWeight()));
+			}
+		}
+
+		/// <summary>
+		/// Function to feed forward the inputs for the neural network
+		/// </summary>
+		/// <param name="prevLayer">previous layer in the neural net</param>
+		public void FeedForward(List<Neuron> prevLayer)
+		{
+			double sum = 0;
+			for(int i = 0; i < prevLayer.Count; i++)
+			{
+				sum += prevLayer[i].m_outputVal
+					* prevLayer[i].m_outputWeights[m_myIndex].Weight;
+			}
+			m_outputVal = TransferFunction(sum);
+		}
+
+		/// <summary>
+		/// Function to calculate the output gradients for a neuron
+		/// </summary>
+		/// <param name="targetVal">target value for neuron</param>
+		public void CalcOutputGradients(double targetVal)
+		{
+            double delta = targetVal - m_outputVal;
+            m_gradient = delta * TransferFunctionDerivative(m_outputVal);
+		}
+
+		/// <summary>
+		/// Calculate the gradients for a hidden neuron
+		/// </summary>
+		/// <param name="nextLayer"></param>
+		public void CalcHiddenGradients(List<Neuron> nextLayer)
+		{
+			double dow = SumDOW(nextLayer);
+			m_gradient = dow * TransferFunctionDerivative(m_outputVal);
+		}
+
+		/// <summary>
+		/// Function to update all of the input weights for the neurons
+		/// </summary>
+		/// <param name="prevLayer">previous layer to allow for updates</param>
+		public void UpdateInputWeights(List<Neuron> prevLayer)
+		{
+			for(int i = 0; i < prevLayer.Count; i++)
+			{
+				Neuron neuron = prevLayer[i];
+				double oldDeltaWeight = neuron.m_outputWeights[m_myIndex].DeltaWeight;
+				double newDeltaWeight =
+                    // Individual input, magnified by the gradient and train rate:
+                    Eta
+                    * neuron.m_outputVal
+					* m_gradient
+                    // Also add momentum = a fraction of the previous delta weight:
+                    + Alpha
+					* oldDeltaWeight;
+
+				neuron.m_outputWeights[m_myIndex].DeltaWeight = newDeltaWeight;
+				neuron.m_outputWeights[m_myIndex].Weight += newDeltaWeight;
+			}
+		}
+
+		/// <summary>
+		/// Function to sum the weights of the next layer
+		/// </summary>
+		/// <param name="nextLayer">next layer of neurons</param>
+		/// <returns>sum of weights</returns>
+		private double SumDOW(List<Neuron> nextLayer)
+		{
+			double sum = 0;
+			for(int i = 0; i < nextLayer.Count - 1; i++)
+			{
+				sum += m_outputWeights[i].Weight * nextLayer[i].m_gradient;
+			}
+			return sum;
+		}
+
+		/// <summary>
+		/// Transfer function for neurons
+		/// </summary>
+		/// <param name="x">sum of weights</param>
+		/// <returns>double after transfer</returns>
+		private static double TransferFunction(double x)
+		{
+			return Math.Tanh(x);
+		}
+
+		/// <summary>
+		/// Derivative of the transfer function
+		/// </summary>
+		/// <param name="x">input value</param>
+		/// <returns>double after transfer derivative</returns>
+		private static double TransferFunctionDerivative(double x)
+		{
+			return 1.0 - x * x;
+		}
+
+		/// <summary>
+		/// Function to generate a random weight between 0 and 1 for Connection init
+		/// </summary>
+		/// <returns>random double between 0 and 1</returns>
+        private static double RandomWeight()
+        {
+			Random rand = new();
+			return rand.NextDouble();
+		}
+	}
+}
+
